049b3e5905314143eb061f01cfaf3d40f216ec4b
Ashutosh <gojiyaashutosh@gmail.com>
2017-07-15 22:15:36 -0400

    reading the mnist label and image files

---
+++ fileio/mnist_read.py
@@ -0,0 +1,38 @@
+import numpy as np
+
+BASE_PATH = 'X:/mnist/{}/'
+IMAGE_WIDTH = IMAGE_HEIGHT = 28
+N_CLASSES = 10
+
+
+def parse_label_file(data_type):
+    path = BASE_PATH.format(data_type) + 'labels.idx1-ubyte'
+    with open(file=path, mode='rb') as file:
+        magic_number = int.from_bytes(file.read(4), byteorder='big')
+        if magic_number != 2049:
+            import sys
+            sys.exit('Invalid file (magic number mismatch)')
+        else:
+            n_labels = int.from_bytes(file.read(4), byteorder='big')
+            buffer = file.read(n_labels)
+            labels = np.frombuffer(buffer=buffer, dtype=np.uint8)
+    return np.eye(N=10)[labels]
+
+
+def parse_image_file(data_type):
+    path = BASE_PATH.format(data_type) + 'images.idx3-ubyte'
+    with open(file=path, mode='rb') as file:
+        magic_number = int.from_bytes(file.read(4), byteorder='big')
+        if magic_number != 2051:
+            import sys
+            sys.exit('Invalid file (magic number mismatch)')
+        else:
+            import os
+            n_images = int.from_bytes(file.read(4), byteorder='big')
+            file.seek(8, os.SEEK_CUR)  # Skip 8 bytes (image width and height already defined)
+            buffer = file.read(n_images * IMAGE_WIDTH * IMAGE_HEIGHT)
+            pixels = np.frombuffer(buffer=buffer, dtype=np.uint8)
+    out = pixels.reshape((n_images, IMAGE_WIDTH, IMAGE_HEIGHT))
+    # In the data set 0=white and 255=black so we have to invert it
+    out = np.abs(out.astype(dtype=np.int8) - 255).astype(dtype=np.uint8)
+    return out
