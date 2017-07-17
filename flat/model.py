479cffc4097a5cf74634b091c8808961340ae99a
Ashutosh <gojiyaashutosh@gmail.com>
2017-07-16 20:37:00 -0400

    planned out the flat (dense) model

---
+++ flat/model.py
@@ -0,0 +1,14 @@
+
+
+def create_flat_model(data_in):
+    """Creates the dense, connected layers and returns the output layer.
+
+    Args:
+        data_in (Tensor): A tensor of shape [1, 784] representing the flattened image (28 * 28 = 784)
+
+    Returns:
+        Tensor: A tensor of shape [1, 10] representing the output of the neural network (10 possible classes)
+
+    Todo:
+        * Create the dense layers and return the final dense layer
+    """
