57b5fbc2b8131c7147a2cdec7b11280066649cd5
Ashutosh <gojiyaashutosh@gmail.com>
2017-07-16 20:18:23 -0400

    making sure that the images and labels correspond and are loaded correctly

--- fileio/mnist_read.py
+++ fileio/mnist_read.py
@@ -1,12 +1,12 @@
 import numpy as np
 
-BASE_PATH = 'X:/mnist/{}/'
+BASE_PATH = 'X:/mnist/{}/{}'
 IMAGE_WIDTH = IMAGE_HEIGHT = 28
 N_CLASSES = 10
 
 
 def parse_label_file(data_type):
-    path = BASE_PATH.format(data_type) + 'labels.idx1-ubyte'
+    path = BASE_PATH.format(data_type, 'labels.idx1-ubyte')
     with open(file=path, mode='rb') as file:
         magic_number = int.from_bytes(file.read(4), byteorder='big')
         if magic_number != 2049:
@@ -20,7 +20,7 @@ def parse_label_file(data_type):
 
 
 def parse_image_file(data_type):
-    path = BASE_PATH.format(data_type) + 'images.idx3-ubyte'
+    path = BASE_PATH.format(data_type, 'images.idx3-ubyte')
     with open(file=path, mode='rb') as file:
         magic_number = int.from_bytes(file.read(4), byteorder='big')
         if magic_number != 2051:
@@ -36,3 +36,14 @@ def parse_image_file(data_type):
     # In the data set 0=white and 255=black so we have to invert it
     out = np.abs(out.astype(dtype=np.int8) - 255).astype(dtype=np.uint8)
     return out
+
+if __name__ == '__main__':
+    images, labels = parse_image_file('train'), parse_label_file('train')
+
+    import random
+    index = random.randrange(0, len(images))
+
+    import cv2
+    cv2.imshow('Training image', images[index])
+    print(np.argmax(labels[index]))
+    cv2.waitKey()
